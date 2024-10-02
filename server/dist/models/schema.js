ec315bc1b086d7157ddbc4739ba980bc969e18f7
Damir Lončarić <damir.loncaric987@gmail.com>
2024-10-02 11:56:17 +0200

    added Trails schema and adjusted user and markers schema

--- server/dist/models/schema.js
+++ server/dist/models/schema.js
@@ -1,30 +1,39 @@
-import mongoose from './index.js';
+import mongoose from "./index.js";
 const Schema = mongoose.Schema;
 const userSchema = new Schema({
-    name: String,
-    email: String,
-    password: String,
+  name: String,
+  email: String,
+  password: String,
+  trails: [TrailSchema],
 });
+
+const TrailSchema = new Schema({
+  name: { type: String, required: true },
+  user_id: { type: String, required: true },
+  userMarkers: [UserMarkersSchema],
+});
+
 const UserMarkersSchema = new mongoose.Schema({
-    _id: { type: String, required: true },
-    user_id: { type: String, required: true },
-    position: {
-        lat: { type: Number, required: true },
-        lng: { type: Number, required: true },
-    },
-    hotel: { type: String },
-    prevDist: {
-        dist: { type: Number, required: true },
-        time: { type: Number, required: true },
-    },
-    nextDist: {
-        dist: { type: Number, required: true },
-        time: { type: Number, required: true },
-    },
-    order: { type: Number },
-    walkingSpeed: { type: Number, required: true },
-    distanceMeasure: { type: String, required: true },
+  _id: { type: String, required: true },
+  user_id: { type: String, required: true },
+  trail_id: { type: String, required: true },
+  position: {
+    lat: { type: Number, required: true },
+    lng: { type: Number, required: true },
+  },
+  hotel: { type: String },
+  prevDist: {
+    dist: { type: Number, required: true },
+    time: { type: Number, required: true },
+  },
+  nextDist: {
+    dist: { type: Number, required: true },
+    time: { type: Number, required: true },
+  },
+  order: { type: Number },
+  walkingSpeed: { type: Number, required: true },
+  distanceMeasure: { type: String, required: true },
 });
-const User = mongoose.model('User', userSchema);
-const UserMarkers = mongoose.model('UserMarkers', UserMarkersSchema);
+const User = mongoose.model("User", userSchema);
+const UserMarkers = mongoose.model("UserMarkers", UserMarkersSchema);
 export { User, UserMarkers };
